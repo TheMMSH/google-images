2b3a78229845ee035109c9b04947d995d1d317bd
themmsh <mohammadmehdish@gmail.com>
2023-10-16 20:51:23 +0330

    chore: hardwiring stuff for achieving deadline

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,60 @@
 package main
 
-func main() {
+import (
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/spf13/viper"
+	"google-images/crypt"
+	"google-images/googleapis"
+	"google-images/img"
+	"google-images/pg"
+	"google-images/service"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
 
+func main() {
+	serv := service.ImageDownloaderService{
+		GoogleApi:    getGoogleApi(),
+		ImageResizer: img.New(viper.GetUint("img.width"), viper.GetUint("img.height")),
+		Cr:           crypt.New(),
+		Repo:         pg.New(getGormDb()),
+	}
+
+	serv.ProcessImagesConcurrently(viper.GetString("query"), viper.GetInt("count"))
+}
+
+func getGoogleApi() googleapis.IGoogleApiService {
+	apiKey := viper.GetString("google.apiKey")
+	sEID := viper.GetString("google.searchEngineId")
+	if apiKey == "" || sEID == "" {
+		log.Fatalf("please ensure that apiKey and searchEngineId exists in config file (use conf/example-config.yaml as an example")
+	}
+	return googleapis.New(apiKey, sEID)
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func setUpViper() {
+	viper.SetConfigName(getEnv("CONFIG_NAME", "config"))
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./conf")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Fatal error config file: %w \n", err)
+	}
+}
+
+func getGormDb() *gorm.DB {
+	db, err := gorm.Open(postgres.Open(viper.GetString("postgresSource")))
+	if err != nil {
+		log.Fatalf("%v failed to initialize gorm DB", err)
+	}
+	return db
 }
